73ee886507f28d25a2628a98c19f329a4ae3e877
rajchatterz <iamraj.space@gmail.com>
2023-10-23 13:28:40 +0530

    done with the html js manipulation

--- chnageBackground.js
+++ chnageBackground.js
@@ -1,2 +1,5 @@
-const element = document.createElement('p')
+const element = document.createElement('button')
+element.textContent = "Press me"
 element.style.padding="20px"
+let body = document.getElementById('body')
+body.appendChild(element)
