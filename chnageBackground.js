0251868b56d3af49e42877f25d27acbcf3793596
rajchatterz <iamraj.space@gmail.com>
2023-10-23 13:48:07 +0530

    donw with basic color changing app

--- chnageBackground.js
+++ chnageBackground.js
@@ -1,5 +1,11 @@
 const element = document.createElement('button')
 element.textContent = "Press me"
-element.style.padding="20px"
+element.style.padding = "20px"
+let color = ["red","green","yellow","purple"]
 let body = document.getElementById('body')
+const head = document.getElementById("col")
 body.appendChild(element)
+element.addEventListener("click", () => {
+    const randomCol = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+    document.body.style.backgroundColor=randomCol
+})
